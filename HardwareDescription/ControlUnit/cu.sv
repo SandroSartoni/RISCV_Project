bdd4e75006e2e517437c97c5f77ae62292785c17
enricorov <enricorov@gmail.com>
2019-07-30 14:26:35 +0200

    Untested release

--- HardwareDescription/ControlUnit/cu.sv
+++ HardwareDescription/ControlUnit/cu.sv
@@ -7,7 +7,7 @@
 
 module cu
 (
-    input logic clk, nrst, stall, chng2nop,
+    input logic posedge clk, nrst, stall, chng2nop,
     input logic [`instr_size-1:0] instr_in,
     output logic [`cw_length-1:0] cw_out
 );
@@ -33,34 +33,41 @@ module cu
 //       ||||||||||||| MUXC_SEL
 //       |||||||||||||| MUXD_SEL
 //       ||||||||||||||| RF_WE
-
-       'b1111111100000,   // branch
-       'b1100111000011,   // jal
-       'b1110101100011,   // jalr
-       'b1110111101001,   // load
-       'b1111111110000,   // store
-       'b1110111100001,   // immediate
-       'b1111011000001,   // rtype
-       'b1111011000001,   // fence    rtype opcode
-       'b1111011000001    // cstype
+                        
+       'b111111110100100,   // branch
+       'b110011100100111,   // jal
+       'b111011010100111,   // jalr
+       'b111011110110101,   // load
+       'b111111111100100,   // store
+       'b111011110100101,   // immediate
+       'b111101100100101,   // rtype
+       'b111101100100101,   // fence    rtype opcode
+       'b111101100100101    // cstype
 
     };
 
-  logic [`opcode_size-1:0] opcode;
-  logic [`cw_length-1:0]  cw1, current_cw;
-  logic [`cw_length-3:0]  cw2;
-  logic [`cw_length-7:0]  cw3;
-  logic [`cw_length-9:0]  cw4;
-  logic [`cw_length-11:0] cw5;
+    logic [`opcode_size-1:0] opcode;
+    logic [`cw_length-1:0]  cw1, current_cw;
+    logic [`cw_length-3:0]  cw2;
+    logic [`cw_length-7:0]  cw3;
+    logic [`cw_length-9:0]  cw4;
+    logic [`cw_length-11:0] cw5;
 
     logic[`regfile_logsize-1:0] rs1_field, rs1_field_previous;		// rs1 for hazards
     logic[`regfile_logsize-1:0] rs2_field, rs2_field_previous;		// rs2 for hazards
     logic[`regfile_logsize-1:0] rd_field, rd_field_previous;		// rd for hazards
+    logic[`opcode_size-1:0] opcode_previous;                        // opcode for hazards
+    
+    assign opcode = instr_in[`opcode_size-1:0];
 
-  assign opcode = instr_in[`opcode_size-1:0];
+    logic F_stall, FD_stall;
+    logic[3:0] counter;
+    logic counting;     // simple flag for starting/resetting the counter
+    
+    statetype state, next_state;
 
-  //super bulky case statement fetching each entry from the internal control word memory_word
-  always_comb begin : cw_fetch
+    //super bulky case statement fetching each entry from the internal control word memory_word
+    always_comb begin : cw_fetch
     case (opcode)
       `branch_group : current_cw = cw_memory[0];
       `jal_op       : current_cw = cw_memory[1];
@@ -72,48 +79,114 @@ module cu
       `fence_op     : current_cw = cw_memory[7];
       `cstype_op    : current_cw = cw_memory[8];
     endcase
-  end : cw_fetch
+    end : cw_fetch
 
-  always_ff @(clk) begin : cw_shift
-    if (stall)
-      cw1 <= 'b0;
-      if (!nrst) begin
-        cw2 <= 'b0;
-        cw3 <= 'b0;
-        cw4 <= 'b0;
-        cw5 <= 'b0;
-      end
+    always_comb begin : fsm_comb
+    case (state)
+        NORMAL :
+            if (FD_stall)
+                next_state = FD_DELAY_ONE;
+            if (F_stall)
+                next_state = F_DELAY_ONE;
+            if (F_stall_mem) 
+                next_state = F_DELAY_MEM;
+                
+        FD_DELAY_ONE :
+            next_state = NORMAL;
+        F_DELAY_ONE:
+            next_state = NORMAL;
+        F_DELAY_MEM :
+            if ( counter == `mem_delay_const )
+                next_state = NORMAL
+    endcase
+    if ( F_stall_mem )
+        counting = 1;
+    
+    if ( counter == `mem_delay_const )
+        counting = 0;
+        
+    end : fsm_comb
+
+    always_ff @(posedge clk) begin : fsm_seq    // including a counter for memory delay
+        
+        if (~nrst)
+            state <= RESET;
+            counter <= 'b0;
+            
+        else 
+            state <= next_state;
+            if ( counting )
+                counter <= counter + 1;
+            else 
+                counter = 'b0
+    end : fsm_seq
+
+    always_ff @(posedge clk) begin : output_logic
+        if (stall)
+            cw1 <= 'b0;
+        if (~nrst) begin
+            cw2 <= 'b0;
+            cw3 <= 'b0;
+            cw4 <= 'b0;
+            cw5 <= 'b0;
+        end
       
-    else begin
-      cw1 <= current_cw;
-      cw3 <= cw2[`cw_length-7:0];
-      cw4 <= cw3[`cw_length-9:0];
-      cw5 <= cw4[`cw_length-11:0];
+        else begin            
+            case(state)
+                NORMAL :
+                    cw1 <= current_cw;
+                    cw3 <= cw2[`cw_length-7:0];
+                    cw4 <= cw3[`cw_length-9:0];
+                    cw5 <= cw4[`cw_length-13:0];
 
-      if(chng2nop)    // the bpu mispredicted
-        cw2 <= 'b0;
-      else
-        cw2 <= cw1[`cw_length-3:0];
-    end
-  end : cw_shift
+                    if(chng2nop)    // the bpu mispredicted
+                        cw2 <= 'b0;
+                    else
+                        cw2 <= cw1[`cw_length-3:0];
+                
+                FD_DELAY : 
+                    cw1 <= {1'b0, cw1[`cw_length-1:0]};    // Disabling PC for one clk
+                    cw2 <= cw2;
+                    cw3 <= 'b0;                            // Bubble
+                    cw4 <= cw2[`cw_length-9:0];
+                    cw5 <= cw4[`cw_length-13:0];
+                
+                F_DELAY :
+                    cw1 <= {1'b0, cw1[`cw_length-1:0]};    // Disabling PC for one clk
+                    cw2 <= 'b0;                            // Bubble
+                    cw3 <= cw1[`cw_length-7:0];
+                    cw4 <= cw3[`cw_length-9:0];
+                    cw5 <= cw4[`cw_length-13:0];
+               
+                F_DELAY_MEM :
+                    cw1 <= {1'b0, cw1[`cw_length-1:0]};    // Disabling PC for one clk
+                    cw2 <= 'b0;                            // Bubble
+                    cw3 <= cw1[`cw_length-7:0];
+                    cw4 <= cw3[`cw_length-9:0];
+                    cw5 <= cw4[`cw_length-13:0];                
+            endcase 
+        end
+    end : output_logic
 
     // Shifting the reg number to store the previous instruction's
 
-    always_ff @(clk) begin : hazard_shift
+    always_ff @(posedge clk) begin : hazard_shift
     
         rs1_field_previous <= rs1_field;
         rs2_field_previous <= rs2_field;
         rd_field_previous <= rd_field;
+        opcode_previous <= opcode;
         
         if (!nrst) begin
             rs1_field_previous <= 'b0;
             rs2_field_previous <= 'b0;
             rd_field_previous <= 'b0;
+            opcode_previous <= 'b0;
         end
     
     end : hazard_shift
 
-    // Decoding the operands to determine the hazards
+    // Decoding the operands for hazard detection
 
     // RegisterSource_1 Field
     always_comb begin : rs1_assign
@@ -141,5 +214,27 @@ module cu
 
 
 // Add the comparison b/w rd_current and previous and the appropriate masking to stall them
+
+    // Logic for having the comparison:
+    
+    always_comb begin : hdu
+        if( (rd_field == rs1_field_previous) || (rd_field == rs2_field_previous) ) begin
+            if ( rd_field != 'b0 ) begin
+                
+                if ( opcode == `ldtype_op )         // First Load then ANY
+                    FD_stall = 1
+                else if ( opcode == `btype_op )     // First Branch then
+                    if ( opcode_previous == `ldtype_op )    // Load
+                        F_stall_mem = 1         // different state for different number of stalls
+                    else                                    // OTHERS
+                        F_stall = 1
+                else 
+                    
+                    FD_stall = 0
+                    F_stall_mem = 0
+                    F_stall = 0
+                end
+            end
+        end
 
 endmodule
