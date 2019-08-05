625389ffd5024bd898b4bac93024088cef61355c
enricorov <enricorov@gmail.com>
2019-08-05 23:10:49 +0200

    Fixed icache load

--- HardwareDescription/ControlUnit/cu.sv
+++ HardwareDescription/ControlUnit/cu.sv
@@ -216,6 +216,7 @@ module cu
                           `rtype_op     : cw1 <= cw_memory[2];
                           `fence_op     : cw1 <= cw_memory[1];
                           `cstype_op    : cw1 <= cw_memory[0];
+                          'b0           : cw1 <= 'b10000000000000;
                         endcase
                         
                         // cw1 <= current_cw;
