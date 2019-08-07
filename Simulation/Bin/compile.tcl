b33c37c107bad181a51667f4b00ab46e86ae193f
enricorov <enricorov@gmail.com>
2019-08-07 09:37:23 +0200

    Included +acc flag, may improve sim speed

--- Simulation/Bin/compile.tcl
+++ Simulation/Bin/compile.tcl
@@ -1,48 +1,50 @@
+# Added te +acc flagg, it may make the simulations run faster, see http://www.pldworld.com/_hdl/2/_ref/se_html/manual_html/c_vlog19.html
+
 set PATH ../../HardwareDescription
 
 # Compile all of the RISCV Modules
 ### Constants ###
-vlog -sv -work work $PATH/Constants/constants.sv
+vlog +acc -sv -work work $PATH/Constants/constants.sv
 #################
 
 ### Fetch Unit ###
 # Branch Prediction Unit
-vlog -sv -work work $PATH/FetchUnit/BranchPredictionUnit/bpu.sv
+vlog +acc -sv -work work $PATH/FetchUnit/BranchPredictionUnit/bpu.sv
 
 # Instruction Cache
-vlog -sv -work work $PATH/FetchUnit/InstructionCache/i_cache.sv
-vlog -sv -work work $PATH/FetchUnit/InstructionCache/icache_controller.sv
+vlog +acc -sv -work work $PATH/FetchUnit/InstructionCache/i_cache.sv
+vlog +acc -sv -work work $PATH/FetchUnit/InstructionCache/icache_controller.sv
 
 # FetchUnit Block
-vlog -sv -work work $PATH/FetchUnit/fetch_unit.sv
+vlog +acc -sv -work work $PATH/FetchUnit/fetch_unit.sv
 ##################
 
 ### Register File ###
-vlog -sv -work work $PATH/RegisterFile/reg_file.sv
+vlog +acc -sv -work work $PATH/RegisterFile/reg_file.sv
 #####################
 
 ### Arithmetic Logic Unit ###
-vlog -sv -work work $PATH/ALU/alu.sv
+vlog +acc -sv -work work $PATH/ALU/alu.sv
 #############################
 
 ### Forward Unit ###
-vlog -sv -work work $PATH/ForwardUnit/forwardunit.sv
+vlog +acc -sv -work work $PATH/ForwardUnit/forwardunit.sv
 #####################
 
 ### Control Unit ###
-vlog -sv -work work $PATH/ControlUnit/cu.sv
+vlog +acc -sv -work work $PATH/ControlUnit/cu.sv
 ##################
 
 ### Data RAM ###
-vlog -sv -work work $PATH/DRAM/dram.sv
+vlog +acc -sv -work work $PATH/DRAM/dram.sv
 ##################
 
 ### RISCV Core ###
-vlog -sv -work work $PATH/RISCVCore/riscv_core.sv
+vlog +acc -sv -work work $PATH/RISCVCore/riscv_core.sv
 ##################
 
 ### RISCV Testbench ###
-vlog -sv -work work $PATH/Testbench/riscv_tester.sv
+vlog +acc -sv -work work $PATH/Testbench/riscv_tester.sv
 #######################
 
 # Launch RISCV Core simulator
